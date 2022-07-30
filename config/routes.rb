6d146864393f6e27cb2ffc4eaec41e17595837fe
tomonari-s <108882451+tomonari-s@users.noreply.github.com>
2022-07-30 13:46:15 +0000

    start 10

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  devise_for :users
+  root to: "homes#top"
+  
+  resources :post_images, only: [:new, :create, :index, :show]
+  
+  get "homes/about" => "homes/about",as:'about'
+end
