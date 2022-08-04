b69fba75ed1ed6415e08383d7b9d861bf51991b1
tomonari-s <108882451+tomonari-s@users.noreply.github.com>
2022-08-04 12:27:17 +0000

    start 22

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -7,8 +7,11 @@ class PostImagesController < ApplicationController
  def create
    @post_image = PostImage.new(post_image_params)
    @post_image.user_id = current_user.id
-   @post_image.save
-   redirect_to post_images_path
+   if @post_image.save
+     redirect_to post_images_path
+   else
+     render :new
+   end
  end
  
 
@@ -17,7 +20,8 @@ class PostImagesController < ApplicationController
  end
 
  def show
-   @post_image = PostImage.find(params[:id]) 
+   @post_image = PostImage.find(params[:id])
+   @post_comment = PostComment.new
  end
 
  def destroy
